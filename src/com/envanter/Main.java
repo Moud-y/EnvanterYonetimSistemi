e2b05c367735df22d9041b5e775142335f60f793
Moudy <mahmoudtbbab33@gmail.com>
2025-12-25 02:16:23 +0300

    test: update Main to use Urun class

--- src/com/envanter/Main.java
+++ src/com/envanter/Main.java
@@ -1,10 +1,18 @@
 package com.envanter;
 
+import com.envanter.model.Urun;
+
 public class Main {
 
-	public static void main(String[] args) {
-       System.out.println("Envanter Yönetim Sistemi baslatildi");
-	}
+    public static void main(String[] args) {
 
+        Urun urun = new Urun(1, "Kalem", 5.0, 3);
+
+        System.out.println("Ürün adı: " + urun.getAd());
+        System.out.println("Stok miktarı: " + urun.getStokMiktari());
+        System.out.println("Düşük stok mu? " + urun.dusukStokMu());
+
+        urun.stokArtir(5);
+        System.out.println("Yeni stok miktarı: " + urun.getStokMiktari());
+    }
 }
-//Day 2 commit marker
